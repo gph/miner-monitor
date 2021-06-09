b509338749a941b7ead00901558d59995eee719d
Henry <gph1990@live.com>
2021-06-09 19:04:07 -0300

    Create monitor.ps1

---
+++ monitor.ps1
@@ -0,0 +1,17 @@
+# Should be in the miner's folder.
+
+While ($true)
+{		
+	$name = "nanominer"
+	$process = Get-Process $name -ErrorAction SilentlyContinue
+	
+	if (!$process) {
+		Write-Output "RUNNING... $(Get-Date -Format '(yyyy/MM/dd HH:mm)')" 
+		
+		Start-Process -Wait -FilePath cmd -ArgumentList "/k .\$name.exe"
+	}	
+	else {
+		Write-Output "RUNNING... $(Get-Date -Format '(yyyy/MM/dd HH:mm)')"
+	}     
+    Start-Sleep 30
+}
