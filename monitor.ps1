eaac62d8e15d651031cc4c34fada370e5adcfe36
Henry <gph1990@live.com>
2021-07-31 16:40:19 -0300

    Update monitor.ps1

--- monitor.ps1
+++ monitor.ps1
@@ -1,17 +1,38 @@
-# Should be in the miner's folder.
+# It should be placed in the miner's folder.
 
-While ($true)
-{		
-	$name = "nanominer"
-	$process = Get-Process $name -ErrorAction SilentlyContinue
-	
-	if (!$process) {
-		Write-Output "STARTING... $(Get-Date -Format '(yyyy/MM/dd HH:mm)')" 
-		
-		Start-Process -Wait -FilePath cmd -ArgumentList "/k .\$name.exe"
-	}	
+$api = "https://api.nanopool.org/v1/eth/"
+$method = "reportedhashrates/"
+$wallet = "CHANGE_WALLET_HASH"
+$url = [System.String]::Concat($api,$method,$wallet)
+$miner = "nanominer"
+$fileConfig = Get-Content -Path config.ini | Select-String -pattern "rigName"
+$rigName = $fileConfig -split "="
+
+$worker = $rigName[1]
+$threshold = "CHANGE_MONITOR_THRESHOLD"
+
+$data = (Invoke-RestMethod -Uri $url | select  data).data
+
+$result = $data | ?{ $_.worker -eq $worker } | Select -ExpandProperty "hashrate"
+
+Function MinerStartUp
+{	
+	if(Get-Process cmd -ErrorAction SilentlyContinue){
+		Stop-Process -Name cmd -Force
+	}
+    	Write-Output "$(Get-Date -Format 'yyyy/MM/dd HH:mm:ss') (MINER STARTED)"
+	Start-Process -FilePath cmd -ArgumentList "/k .\$miner.exe"
+}
+
+while($true){
+
+	if($result -lt $threshold){
+		Write-Output "$(Get-Date -Format 'yyyy/MM/dd HH:mm:ss') $result (DOWN)"
+        MinerStartUp
+	}
 	else {
-		Write-Output "RUNNING... $(Get-Date -Format '(yyyy/MM/dd HH:mm)')"
-	}     
-    Start-Sleep 30
+		Write-Output "$(Get-Date -Format 'yyyy/MM/dd HH:mm:ss') $result (UP)"
+		
+	}
+	Start-Sleep -s 300
 }
