36d7bc92f35e57529cd4a77fe71aafec5770c49d
Henry <gph1990@live.com>
2021-08-03 01:12:58 -0300

    Updated
    way better

--- monitor.ps1
+++ monitor.ps1
@@ -1,38 +1,20 @@
 # It should be placed in the miner's folder.
 
-$api = "https://api.nanopool.org/v1/eth/"
-$method = "reportedhashrates/"
-$wallet = "CHANGE_WALLET_HASH"
-$url = [System.String]::Concat($api,$method,$wallet)
-$miner = "nanominer"
-$fileConfig = Get-Content -Path config.ini | Select-String -pattern "rigName"
-$rigName = $fileConfig -split "="
-
-$worker = $rigName[1]
-$threshold = "CHANGE_MONITOR_THRESHOLD"
-
-$data = (Invoke-RestMethod -Uri $url | select  data).data
-
-$result = $data | ?{ $_.worker -eq $worker } | Select -ExpandProperty "hashrate"
-
 Function MinerStartUp
 {	
 	if(Get-Process cmd -ErrorAction SilentlyContinue){
 		Stop-Process -Name cmd -Force
 	}
-    	Write-Output "$(Get-Date -Format 'yyyy/MM/dd HH:mm:ss') (MINER STARTED)"
-	Start-Process -FilePath cmd -ArgumentList "/k .\$miner.exe"
+    Write-Output "$(Get-Date -Format 'yyyy/MM/dd HH:mm:ss') (MINER STARTED)"
+	Start-Process -FilePath cmd -ArgumentList "/k .\nanominer.exe"
 }
 
 while($true){
-
-	if($result -lt $threshold){
-		Write-Output "$(Get-Date -Format 'yyyy/MM/dd HH:mm:ss') $result (DOWN)"
+    if(Get-Process nanominer -ErrorAction SilentlyContinue){
+        Write-Output "$(Get-Date -Format 'yyyy/MM/dd HH:mm:ss') (UP)"
+    }else {
+		Write-Output "$(Get-Date -Format 'yyyy/MM/dd HH:mm:ss') (DOWN)"
         MinerStartUp
 	}
-	else {
-		Write-Output "$(Get-Date -Format 'yyyy/MM/dd HH:mm:ss') $result (UP)"
-		
-	}
-	Start-Sleep -s 900
+	Start-Sleep -s 15
 }
