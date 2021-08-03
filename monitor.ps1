4c4506edb4ce4746ff505b7bfd671864aa485079
Henry <gph1990@live.com>
2021-08-03 01:15:10 -0300

    Update monitor.ps1

--- monitor.ps1
+++ monitor.ps1
@@ -5,16 +5,17 @@ Function MinerStartUp
 	if(Get-Process cmd -ErrorAction SilentlyContinue){
 		Stop-Process -Name cmd -Force
 	}
-    Write-Output "$(Get-Date -Format 'yyyy/MM/dd HH:mm:ss') (MINER STARTED)"
+    	Write-Output "$(Get-Date -Format 'yyyy/MM/dd HH:mm:ss') (MINER STARTED)"
 	Start-Process -FilePath cmd -ArgumentList "/k .\nanominer.exe"
 }
 
 while($true){
-    if(Get-Process nanominer -ErrorAction SilentlyContinue){
-        Write-Output "$(Get-Date -Format 'yyyy/MM/dd HH:mm:ss') (UP)"
-    }else {
+	if(Get-Process nanominer -ErrorAction SilentlyContinue){
+        	Write-Output "$(Get-Date -Format 'yyyy/MM/dd HH:mm:ss') (UP)"
+   	}
+	else {
 		Write-Output "$(Get-Date -Format 'yyyy/MM/dd HH:mm:ss') (DOWN)"
-        MinerStartUp
+        	MinerStartUp
 	}
 	Start-Sleep -s 15
 }
